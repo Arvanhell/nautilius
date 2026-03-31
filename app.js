ffde8c8e66b8ff96b7871a671a2efd3c81233ac3
Arvanhell <arrivald@gmail.com>
2026-03-31 22:17:49 +0100

    UI: Final polish - layoiut stable, scale perfect, auto-complete off

--- app.js
+++ app.js
@@ -5,9 +5,8 @@ document.body.style.border = "5px solid #00ff88"; // Neon frame
 */
 const mantra = "I am not dust in the machine, but the spark that drives it. My body is an antena , my myscles are a transmitter. With every moment, I draw the Source energy deep into the matter.Weight is not the enemy - it is the resistance that builds my freedom. Two decades on the road, returning on my terms. Today I build the foundation for the world that is comming. I am ready. I am Power. I am Truth. ";
 
-const displayElement = document.createElement('div');
-displayElement.id = "mantra-terminal";
-document.body.appendChild(displayElement);
+const displayElement = document.getElementById('mantra-text'); 
+
 
 let index = 0;
 
